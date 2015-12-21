53dd90f7ebb42b1cbfd28479397d6a6d49b9ad08
Vitor Alano de Ataides <alano.vitor@gmail.com>
2015-12-20 22:59:10 -0200

    Somes services

--- db/schemas/poll.js
+++ db/schemas/poll.js
@@ -1,5 +1,7 @@
 var schema = { 
     name: { type: String, required: true },
+    description: { type: String, required: true},
+    owner: { type: String, required: true},
     hash: { type: String, required: true }
 };
 
